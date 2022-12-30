fa1ac5b1689f7095af6b87e77906736007d09a2d
Wilbert Heinen <wilbert.heinen@gmail.com>
2022-12-30 15:07:10 +0100

    New year new style

--- Gemfile
+++ Gemfile
@@ -2,5 +2,5 @@ source 'https://rubygems.org'
 gem "bundler"
 gem "jekyll"
 gem "jekyll-paginate"
-gem 'jekyll-tagging'
 gem "jekyll-sitemap"
+gem "jekyll", "~> 4.0"
--- js/common.js
+++ js/common.js
@@ -1,114 +1,173 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function() {
   'use strict';
 
-  var headerOverlay = $(".header__overlay"),
-    menuOpenIcon = $(".nav__icon-menu"),
-    menuCloseIcon = $(".ion-md-close"),
-    menuList = $(".main-nav"),
-    searchOpenIcon = $(".nav__icon-search"),
-    searchCloseIcon = $(".search__close"),
-    searchBox = $(".search"),
-    searchInput = $(".search__text");
+  var html = document.querySelector('html'),
+    globalWrap = document.querySelector('.global-wrap'),
+    body = document.querySelector('body'),
+    menuToggle = document.querySelector(".hamburger"),
+    menuList = document.querySelector(".main-nav"),
+    menuList = document.querySelector(".main-nav"),
+    searchOpenIcon = document.querySelector(".icon__search"),
+    searchCloseIcon = document.querySelector(".icon__search__close"),
+    searchOverlay = document.querySelector(".search__overlay"),
+    searchInput = document.querySelector(".search__text"),
+    search = document.querySelector(".search"),
+    toggleTheme = document.querySelector(".toggle-theme"),
+    btnScrollToTop = document.querySelector(".top");
 
 
-  /* =======================
-  // Menu and Search
-  ======================= */
-  menuOpenIcon.click(function() {
-    menuOpen();
-  })
+  /* =======================================================
+  // Menu + Search + Theme Switcher
+  ======================================================= */
+  menuToggle.addEventListener("click", () => {
+    menu();
+  });
 
-  menuCloseIcon.click(function () {
-    menuClose();
-  })
-
-  searchOpenIcon.click(function () {
+  searchOpenIcon.addEventListener("click", () => {
     searchOpen();
   });
 
-  searchCloseIcon.click(function () {
+  searchCloseIcon.addEventListener("click", () => {
     searchClose();
   });
 
-  headerOverlay.click(function () {
-    menuClose();
+  searchOverlay.addEventListener("click", () => {
     searchClose();
   });
 
-  function menuOpen() {
-    menuList.addClass("is-open");
-    headerOverlay.addClass("is-visible");
+  // Menu
+  function menu() {
+    menuToggle.classList.toggle("is-open");
+    menuList.classList.toggle("is-visible");
   }
 
-  function menuClose() {
-    menuList.removeClass("is-open");
-    headerOverlay.removeClass("is-visible");
-  }
-
+  // Search
   function searchOpen() {
-    searchBox.addClass("is-visible");
+    search.classList.add("is-visible");
+    body.classList.add("search-is-visible");
+    globalWrap.classList.add("is-active");
+    menuToggle.classList.remove("is-open");
+    menuList.classList.remove("is-visible");
     setTimeout(function () {
       searchInput.focus();
-    }, 150);
+    }, 250);
   }
 
   function searchClose() {
-    searchBox.removeClass("is-visible");
+    search.classList.remove("is-visible");
+    body.classList.remove("search-is-visible");
+    globalWrap.classList.remove("is-active");
   }
 
-  $('.search, .search__box').on('click keyup', function(event) {
-    if (event.target == this || event.keyCode == 27) {
-      $('.search').removeClass('is-visible');
+  document.addEventListener('keydown', function(e){
+    if (e.key == 'Escape') {
+      searchClose();
     }
   });
 
+  // Theme Switcher
+  if (toggleTheme) {
+    toggleTheme.addEventListener("click", () => {
+      darkMode();
+    });
+  };
 
-  /* =======================
+  function darkMode() {
+    if (html.classList.contains('dark-mode')) {
+      html.classList.remove('dark-mode');
+      localStorage.removeItem("theme");
+      document.documentElement.removeAttribute("dark");
+    } else {
+      html.classList.add('dark-mode');
+      localStorage.setItem("theme", "dark");
+      document.documentElement.setAttribute("dark", "");
+    }
+  };
+
+
+  // =====================
   // Simple Jekyll Search
-  ======================= */
+  // =====================
   SimpleJekyllSearch({
     searchInput: document.getElementById("js-search-input"),
     resultsContainer: document.getElementById("js-results-container"),
     json: "/search.json",
-    searchResultTemplate: '{article}',
-    noResultsText: '<h3 class="no-results">No results found</h3>'
+    searchResultTemplate: '<div class="search-results__item"><a href="{url}" class="search-results__image" style="background-image: url({image})"></a> <a href="{url}" class="search-results__link"><time class="search-results-date" datetime="{date}">{date}</time><div class="search-results-title">{title}</div></a></div>',
+    noResultsText: '<h4 class="no-results">No results found</h4>'
   });
 
-  /* =======================
-  // LazyLoad Images
-  ======================= */
-  var lazyLoadInstance = new LazyLoad({
-    elements_selector: '.lazy'
-  })
+
+  /* ================================================================
+  // Stop Animations During Window Resizing and Switching Theme Modes
+  ================================================================ */
+  let disableTransition;
+
+  if (toggleTheme) {
+    toggleTheme.addEventListener("click", () => {
+      stopAnimation();
+    });
+
+    window.addEventListener("resize", () => {
+      stopAnimation();
+    });
+
+    function stopAnimation() {
+      document.body.classList.add("disable-animation");
+      clearTimeout(disableTransition);
+      disableTransition = setTimeout(() => {
+        document.body.classList.remove("disable-animation");
+      }, 100);
+    }
+  }
 
 
   /* =======================
   // Responsive Videos
   ======================= */
-  $(".post__content, .page__content").fitVids({
-    customSelector: ['iframe[src*="ted.com"]', 'iframe[src*="facebook.com"]']
-  });
+  reframe(".post__content iframe:not(.reframe-off), .page__content iframe:not(.reframe-off)");
+
+
+  /* =======================
+  // LazyLoad Images
+  ======================= */
+  var lazyLoadInstance = new LazyLoad({
+    elements_selector: ".lazy"
+  })
 
 
   /* =======================
   // Zoom Image
   ======================= */
-  $('.post p:has(> img), .page p:has(> img)').addClass("image-box");
-  $(".page img, .post img, .gallery__image img").attr("data-action", "zoom");
-  $(".page a img, .post a img").removeAttr("data-action", "zoom");
+  const lightense = document.querySelector(".page__content img, .post__content img, .gallery__image img"),
+  imageLink = document.querySelectorAll(".page__content a img, .post__content a img, .gallery__image a img");
+
+  if (imageLink) {
+    for (var i = 0; i < imageLink.length; i++) imageLink[i].parentNode.classList.add("image-link");
+    for (var i = 0; i < imageLink.length; i++) imageLink[i].classList.add("no-lightense");
+  }
+
+  if (lightense) {
+    Lightense(".page__content img:not(.no-lightense), .post__content img:not(.no-lightense), .gallery__image img:not(.no-lightense)", {
+    padding: 60,
+    offset: 30
+    });
+  }
 
 
   /* =======================
   // Scroll Top Button
   ======================= */
-  $(".top").click(function() {
-    $("html, body").stop().animate({ scrollTop: 0 }, "slow", "swing");
+  window.addEventListener("scroll", function () {
+    window.scrollY > window.innerHeight ? btnScrollToTop.classList.add("is-active") : btnScrollToTop.classList.remove("is-active");
   });
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > $(window).height()) {
-      $(".top").addClass("is-active");
-    } else {
-      $(".top").removeClass("is-active");
+
+  btnScrollToTop.addEventListener("click", function () {
+    if (window.scrollY != 0) {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth"
+      })
     }
   });
 
